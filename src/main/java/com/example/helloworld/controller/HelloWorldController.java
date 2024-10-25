abb3d5e81defc65195b33befb54cfa8fcb07e193
Junior Carrion <juniorcr2198@gmail.com>
2024-10-25 11:54:03 -0500

    feat: add ping to jira

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,13 +1,34 @@
 package com.example.helloworld.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWorldController {
 
+    private final RestTemplate restTemplate;
+
+    public HelloWorldController() {
+        this.restTemplate = new RestTemplate();
+    }
+
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World!";
+        boolean ping1 = pingUrl("https://bcp-ti.atlassian.net.mcas.ms/jira/");
+        boolean ping2 = pingUrl("https://bcp-ti.atlassian.net/jira/");
+        return "Hello url1: " + ping1 + "url2" + ping2;
+    }
+
+    public boolean pingUrl(String url) {
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            return response.getStatusCode() == HttpStatus.OK || response.getStatusCode() == HttpStatus.UNAUTHORIZED;
+        } catch (Exception e) {
+            System.out.println("error: " + e.toString());
+            return false;
+        }
     }
 }
